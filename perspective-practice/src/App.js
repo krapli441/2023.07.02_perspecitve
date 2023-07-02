d23323e69fe36277011bb0da6eb6446aea2c8ef2
krapli441 <tlfqj1208@gmail.com>
2023-07-02 15:16:20 +0900

    예제 작성

--- perspective-practice/src/App.js
+++ perspective-practice/src/App.js
@@ -1,23 +1,10 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          반갑다. <code>Creat-React-App</code>를 이용해서 앱을 만들었다.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="box"></div>
     </div>
   );
 }
