be32d109ab8ffba433c82353575ac079501ffdec
krapli441 <tlfqj1208@gmail.com>
2023-07-02 15:38:09 +0900

    우선 효과는 구현 성공.

--- perspective-practice/src/App.js
+++ perspective-practice/src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-export default function MousePositionEffect() {
+export default function MovingBoxEffect() {
+  const containerRef = useRef(null);
+  const [boxPosition, setBoxPosition] = useState({ x: 0, y: 0 });
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (event) => {
@@ -8,17 +10,49 @@ export default function MousePositionEffect() {
     setMousePosition({ x: clientX, y: clientY });
   };
 
+  useEffect(() => {
+    const containerSize = containerRef.current.offsetWidth;
+    const boxSize = 100;
+    const maxOffset = containerSize / 2 - boxSize / 2;
+    const sensitivity = 0.1;
+
+    const moveBox = () => {
+      const offsetX = (mousePosition.x - containerSize / 2) * sensitivity;
+      const offsetY = (mousePosition.y - containerSize / 2) * sensitivity;
+      const newX = Math.min(Math.max(-maxOffset, offsetX), maxOffset);
+      const newY = Math.min(Math.max(-maxOffset, offsetY), maxOffset);
+      setBoxPosition({ x: newX, y: newY });
+    };
+
+    moveBox();
+  }, [mousePosition]);
+
+  useEffect(() => {
+    setMousePosition({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+  }, []);
+
   return (
     <div
+      ref={containerRef}
       style={{
-        width: "200px",
-        height: "200px",
-        background: "red",
+        width: "100vw",
+        height: "100vh",
+        background: "lightgray",
         position: "relative",
-        transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
-        transition: "transform 0.2s ease",
       }}
       onMouseMove={handleMouseMove}
-    ></div>
+    >
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          background: "red",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          transform: `translate(${boxPosition.x}px, ${boxPosition.y}px)`,
+        }}
+      ></div>
+    </div>
   );
 }
